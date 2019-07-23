d411309e7006660e5156db9857906d4aae86ffe8
Cassio Almeron <50529323+cassioalmeron@users.noreply.github.com>
2019-07-22 22:36:06 -0300

    Conclusão Módulo 05

--- Modulo05/src/pages/Main/index.js
+++ Modulo05/src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 import api from "../../services/api";
 
-import { Container, Form, SubmitButton, List } from "./styles";
+import { Container } from "../../components/Container";
+import { Form, SubmitButton, List } from "./styles";
 
 export default class Main extends Component {
   state = {
@@ -79,10 +81,12 @@ export default class Main extends Component {
         </Form>
 
         <List>
-          {repositories.map(x => (
-            <li key={x.name}>
-              <span>{x.name}</span>
-              <a href="">Detalhes</a>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
